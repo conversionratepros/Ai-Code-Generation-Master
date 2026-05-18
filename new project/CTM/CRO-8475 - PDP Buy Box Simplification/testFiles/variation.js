6faac10fa2be792552ce1d0ff54d8051d96a2c02
Mahbub-elahi-1 <mahbub@conversionratepros.co.za>
2026-05-18 15:14:25 +0600

    Update Code

--- new project/CTM/CRO-8475 - PDP Buy Box Simplification/testFiles/variation.js
+++ new project/CTM/CRO-8475 - PDP Buy Box Simplification/testFiles/variation.js
@@ -4,7 +4,7 @@
         var debug = 0;
         var variation_name = "CRO-8475";
         /* all Pure helper functions */
-
+        
         function waitForElement(selector, trigger) {
             var interval = setInterval(function () {
                 if (
@@ -20,7 +20,7 @@
                 clearInterval(interval);
             }, 15000);
         }
-
+        
         function live(selector, event, callback, context) {
             function addEvent(el, type, handler) {
                 if (el.attachEvent) el.attachEvent("on" + type, handler);
@@ -52,7 +52,7 @@
             }
             live(selector, event, callback, context);
         }
-
+        
         function insertHtml(selector, content, position) {
             var el = document.querySelector(selector);
             if (!position) {
@@ -62,42 +62,42 @@
                 el.insertAdjacentHTML(position, content);
             }
         }
-
+        
         function innerHTMLContent(selector, content) {
             var el = document.querySelector(selector);
             if (el) {
                 el.innerHTML = content;
             }
         }
-
+        
         function innerChildContent(selector, childNumber, content) {
             var el = document.querySelector(selector);
             if (el.hasChildNodes()) {
                 el.childNodes[childNumber].textContent = content;
             }
         }
-
+        
         function addClass(el, cls) {
             var el = document.querySelector(el);
             if (el) {
                 el.classList.add(cls);
             }
         }
-
+        
         function toggleClass(el, cls) {
             var el = document.querySelector(el);
             if (el) {
                 el.classList.toggle(cls);
             }
         }
-
+        
         function removeClass(el, cls) {
             var el = document.querySelector(el);
             if (el) {
                 el.classList.contains(cls) && el.classList.remove(cls);
             }
         }
-
+        
         function scroll(click, selector) {
             click.addEventListener('click', function (event) {
                 event.preventDefault();
@@ -112,26 +112,38 @@
         }
 
         function updateVASIcons() {
-            var newIcons = {
-                ".modal_1 .vas-modal_icon": "https://crp-clients-images.s3.af-south-1.amazonaws.com/CTM/PDP+%7C+Buy+Box+simplification+%7C+All+%7C+CRO-8475/CRO-8475-1.svg",
-                ".modal_2 .vas-modal_icon": "https://crp-clients-images.s3.af-south-1.amazonaws.com/CTM/PDP+%7C+Buy+Box+simplification+%7C+All+%7C+CRO-8475/CRO-8475-2.svg",
-                ".modal_3 .vas-modal_icon": "https://crp-clients-images.s3.af-south-1.amazonaws.com/CTM/PDP+%7C+Buy+Box+simplification+%7C+All+%7C+CRO-8475/CRO-8475-3.svg",
-                ".modal_4 .vas-modal_icon": "https://crp-clients-images.s3.af-south-1.amazonaws.com/CTM/PDP+%7C+Buy+Box+simplification+%7C+All+%7C+CRO-8475/CRO-8475-4.svg"
-            };
 
-            Object.keys(newIcons).forEach(function(selector) {
-                var img = document.querySelector(selector);
-                if (img) {
-                    img.src = newIcons[selector];
-                }
-            });
-        }
+  const newIcons = {
+    ".modal_1 .vas-modal_icon":
+      "https://crp-clients-images.s3.af-south-1.amazonaws.com/CTM/PDP+%7C+Buy+Box+simplification+%7C+All+%7C+CRO-8475/CRO-8475-1.svg",
 
+    ".modal_2 .vas-modal_icon":
+      "https://crp-clients-images.s3.af-south-1.amazonaws.com/CTM/PDP+%7C+Buy+Box+simplification+%7C+All+%7C+CRO-8475/CRO-8475-2.svg",
+
+    ".modal_3 .vas-modal_icon":
+      "https://crp-clients-images.s3.af-south-1.amazonaws.com/CTM/PDP+%7C+Buy+Box+simplification+%7C+All+%7C+CRO-8475/CRO-8475-3.svg",
+
+    ".modal_4 .vas-modal_icon":
+      "https://crp-clients-images.s3.af-south-1.amazonaws.com/CTM/PDP+%7C+Buy+Box+simplification+%7C+All+%7C+CRO-8475/CRO-8475-4.svg"
+  };
+
+  Object.keys(newIcons).forEach(function(selector) {
+
+    const img = document.querySelector(selector);
+
+    if (img) {
+      img.src = newIcons[selector];
+    }
+
+  });
+
+}
+        
         function init() {
             updateVASIcons();
             addClass("body", variation_name);
         }
-
+        
         waitForElement('body', init);
     } catch (e) {
         if (debug) console.log(e, "error in Test" + variation_name);
